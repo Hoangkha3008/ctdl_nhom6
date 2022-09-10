6de16c248fb63b2ad0e7af6c50dce75ce5d4a315
hoangkha3008 <hoangkha3008@gmail.com>
2022-09-10 19:30:52 +0700

    initial commit

---
+++ LIST_ARRAY_LIST111_detai15/DEMO_LIST_ARRAY_LIST111/DEMO_LIST_ARRAY_LIST/DEMO_LIST_ARRAY_LIST/Demo_List/Demo_List/Array_list.cs
@@ -0,0 +1,323 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Collections;
+//namespace Demo_List
+//{
+//    class Array_list
+//    {
+//        static void khaibaokhoitaoarraylist()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//        }
+//        static void chencacphantu()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            // Chèn các phần tử vào Arraylist
+//            Console.WriteLine("\nChen phan tu vao ArrayList: ");
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+
+
+       
+//        static void thietlapphantu()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Thiết lập số phần tử mà Arraylist có thể chứa
+//            Console.WriteLine("\nDung luong cua ArrayList: {0} ",
+//                arraylist.Capacity);
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+//        static void sophantuthucsuchua()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Số lượng phần tử thực sự được chứa trong Arraylist
+//            Console.WriteLine("So phan tu trong ArrayList: {0}",
+//                arraylist.Count);
+//            Console.WriteLine("\nHien thi cac phan tu trong ArrayList: ");
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+//        static void insertphantu()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Insert phan tu 
+//            Console.WriteLine("Insert phan tu :");
+//            arraylist.Insert(10, 3); // in 3 ở vị trí thứ 10
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+
+//            Console.WriteLine("\n");
+//        }
+//        static void daonguoctatca()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Đảo ngược tất cả các phần tử
+//            Console.WriteLine("Dao nguoc tat ca cac phan tu :");
+//            arraylist.Reverse();
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+//        static void sapxeptheothututang()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Sắp xếp theo thứ tự tăng dần 
+//            Console.WriteLine("\nSap xep va hien thi ArrayList " +
+//                "sau khi da sap xep: ");
+//            arraylist.Sort();
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+//        static void xoaphantu()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Xóa phần tử chỉ định
+//            Console.WriteLine("Xoa phan tu tai vi tri so 7 :");
+//            arraylist.RemoveAt(7);
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+//        static void kiemtra()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Kiễm tra phần tử 
+//            Console.WriteLine("Kiem tra phan tu :");
+//            bool kiemtra = arraylist.Contains(2);
+
+//            if (kiemtra == false)
+//            {
+//                Console.WriteLine("Khong tim thay phan tu trong Arraylist");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Tim thay phan tu trong Arraylist");
+//            }
+//            Console.WriteLine();
+//        }
+//        static void taobansao()
+//        {
+//            // Tạo 1 Arraylist mới 
+//            ArrayList arraylist = new ArrayList();
+//            arraylist.Add(1);
+//            arraylist.Add(7);
+//            arraylist.Add(10);
+//            arraylist.Add(4);
+//            arraylist.Add(3);
+//            arraylist.Add(6);
+//            arraylist.Add(7);
+//            arraylist.Add(2);
+//            arraylist.Add(5);
+//            arraylist.Add(9);
+//            arraylist.Add(8);
+//            // Tạo bản sao từ Array hiện tại 
+//            Console.WriteLine("Ban sao moi:");
+//            arraylist.Clone();
+//            foreach (int i in arraylist)
+//            {
+//                Console.Write(i + " ");
+//            }
+//            Console.WriteLine("\n");
+//        }
+       
+//        static void Main(string[] args)
+//        {
+//            int chon;
+//            do
+//            {
+
+//                Console.WriteLine("***************MAIN MENU**********");
+//                Console.WriteLine("*******Chuong trinh minh hoa cho Array List********");
+//                Console.WriteLine("1.   Khai bao va khoi tao Array List");
+//                Console.WriteLine("2.   Chen cac phan tu trong Array List");
+//                Console.WriteLine("3.   Thiet lap phan tu");
+//                Console.WriteLine("4.   So phan tu thuc su duoc chua");
+//                Console.WriteLine("5.   Insert phan tu");
+//                Console.WriteLine("6.   Dao nguoc tat ca cac phan tu");
+//                Console.WriteLine("7.   sap xep theo thu tu tang dan");
+//                Console.WriteLine("8.   xoa phan tu chi dinh");
+//                Console.WriteLine("9.   kiem tra phan tu");
+//                Console.WriteLine("10.   tao ban sao tu array hien tai");
+//                Console.Write("    NHAP LUA  CHON CUA BAN VAO:");
+
+//                chon = int.Parse(Console.ReadLine());
+//                switch (chon)
+//                {
+//                    case 1:
+//                        khaibaokhoitaoarraylist();
+//                        break;
+//                    case 2:
+
+
+//                        chencacphantu();
+//                        break;
+
+//                    case 3:
+//                        thietlapphantu();
+//                        break;
+
+//                    case 4:
+//                        sophantuthucsuchua();
+//                        break;
+//                    case 5:
+//                        insertphantu();
+//                        break;
+//                    case 6:
+//                        daonguoctatca();
+//                        break;
+//                    case 7:
+//                        sapxeptheothututang();
+//                        break;
+//                    case 8:
+//                        xoaphantu();
+//                        break;
+//                    case 9:
+//                        kiemtra();
+//                        break;
+//                    case 10:
+//                        taobansao();
+//                        break;
+//                    default:
+//                        Console.WriteLine("vui long nhap lai");
+//                        break;
+//                }
+//            } while (chon != 11);
+//            Console.ReadLine();
+//        }
+//    }
+//}
+    
+           
+            
+            
+            
+
+           
+           
+            
+            
+            
+           
+           
+           
+
+       
---
+++ LIST_ARRAY_LIST111_detai15/DEMO_LIST_ARRAY_LIST111/DEMO_LIST_ARRAY_LIST/DEMO_LIST_ARRAY_LIST/Demo_List/Demo_List/list.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListTrongCSharp
+{
+    class list
+    {
+
+        static void khaibaokhoitaolist()
+        {
+            // Khai báo và khởi tạo List
+
+            List<int> List = new List<int>(); // khởi tạo 1 List các số nguyên rỗng
+
+            Console.WriteLine("Danh sach so nguyen :\n ");
+            List<int> List2 = new List<int>() { 7, 8, 9, 10, 5, 3, 4, 1 }; // khởi tạo 1 List các số nguyên
+            foreach (var item in List2)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine("\n");
+
+
+
+        }
+        static void travecacphantutrongdanhsach()
+        {
+            Console.WriteLine("In các phan tu lon hon 5:\n");
+            List<int> List2 = new List<int>() { 7, 8, 9, 10, 5, 3, 4, 1 };
+            //FindAll trả về tất cả các phần tử trong danh sách
+            var t = List2.FindAll(
+                (e) => { return e > 5; }
+                );
+            foreach (var n in t)
+            {
+                Console.Write(n);
+            }
+            Console.WriteLine();
+        }
+        static void duyetcacphantucuamang()
+        {
+            //Duyệt các phần tử của mảng
+            List<int> List2 = new List<int>() { 7, 8, 9, 10, 5, 3, 4, 1 };
+            Console.WriteLine("\n");
+            Console.WriteLine("Duyet cac phan tu cua mang:");
+            for (var i = 0; i < List2.Count; i++)
+            {
+                Console.WriteLine($"{ List2[i]}\t");
+            }
+
+            Console.WriteLine();
+        }
+        static void sapxeptheothututangdan()
+        {
+            List<int> List2 = new List<int>() { 7, 8, 9, 10, 5, 3, 4, 1 };
+            // Sắp xếp theo thứ tự tăng dần
+            Console.WriteLine("Sap xep tang  :\n ");
+            List2.Sort();
+            foreach (var item in List2)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+        static void inphantutronglist()
+        {
+            //  Tạo 1 List các kiểu string và thêm 2 phần tử vào List.
+
+
+            List<string> List4 = new List<string>();
+            List4.Add("NguyenHoangKha \n");
+            List4.Add("LeHoangNhut \n");
+            List4.Add("NguyenTrongLiem \n");
+            List4.Add("NguyenQuocHoang Son \n");
+            List4.Add("NguyenMinhTriet \n");
+
+            // In giá trị các phần tử trong List
+            Console.WriteLine(" List ban dau: ");
+            Console.WriteLine(" So luong phan tu trong List la: {0}", List4.Count);
+            foreach (string item in List4)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+        static void ingiatriduocchen()
+        {
+            List<string> List4 = new List<string>();
+            List4.Add("NguyenHoangKha \n");
+            List4.Add("LeHoangNhut \n");
+            List4.Add("NguyenTrongLiem \n");
+            List4.Add("NguyenQuocHoang Son \n");
+            List4.Add("NguyenMinhTriet \n");
+            // Chèn 1 phần tử vào đầu List.
+            List4.Insert(0, "NguyenVanA \n");
+            // In lại giá trị các phần tử trong List để xem đã chèn được hay chưa
+            Console.WriteLine(" List sau khi insert: ");
+            Console.WriteLine(" So luong phan tu trong List la: {0}", List4.Count);
+            foreach (string item in List4)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine();
+        }
+        static void kiemtra1phantutronglistcotontaihayko()
+        {
+            List<string> List4 = new List<string>();
+            List4.Add("NguyenHoangKha \n");
+            List4.Add("LeHoangNhut \n");
+            List4.Add("NguyenTrongLiem \n");
+            List4.Add("NguyenQuocHoang Son \n");
+            List4.Add("NguyenMinhTriet \n");
+            // Kiểm tra 1 phần tử có tồn tại trong List hay không.
+            bool isExists = List4.Contains("NguyenTrongLiem \n");
+
+            if (!isExists)
+            {
+                Console.WriteLine("Khong tim thay chuoi trong List");
+            }
+            else
+            {
+                Console.WriteLine("Tim thay chuoi trong List");
+            }
+        }
+        static void xoaphantu()
+        {
+            List<string> List4 = new List<string>();
+            List4.Add("NguyenHoangKha \n");
+            List4.Add("LeHoangNhut \n");
+            List4.Add("NguyenTrongLiem \n");
+            List4.Add("NguyenQuocHoang Son \n");
+            List4.Add("NguyenMinhTriet \n");
+            // xoa phan tu 
+            Console.WriteLine("Xoa phan tu :\n");
+            List4.Remove("NguyenTrongLiem \n");
+            foreach (string item in List4)
+            {
+                Console.Write(" " + item);
+            }
+            Console.WriteLine("\n");
+        }
+        static void daonguocphantu()
+        {
+            List<string> List4 = new List<string>();
+            List4.Add("NguyenHoangKha \n");
+            List4.Add("LeHoangNhut \n");
+            List4.Add("NguyenTrongLiem \n");
+            List4.Add("NguyenQuocHoang Son \n");
+            List4.Add("NguyenMinhTriet \n");
+            //Đảo ngược các phần tử trong List
+            Console.WriteLine("Dao nguoc tat ca cac phan tu :\n");
+            List4.Reverse();
+            foreach (string item in List4)
+            {
+                Console.Write(" " + item);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static void Main(string[] args)
+        {
+            int chon;
+            do
+            {
+
+                Console.WriteLine("***************MAIN MENU**********");
+                Console.WriteLine("*******Chuong trinh minh hoa cho List********");
+                Console.WriteLine("1.   KHAI BAO VA KHOI TAO LIST");
+                Console.WriteLine("2.   TRA VE TAT CA CAC PHAN TU TRONG DANH SACH");
+                Console.WriteLine("3.   DUYET CAC PHAN TU CUA MANG");
+                Console.WriteLine("4.   SAP XEP THEO THU TU TANG DAN");
+                Console.WriteLine("5.   IN GIA TRI CAC PHAN TU TRONG LIST");
+                Console.WriteLine("6.   IN GIA TRI CAC PHAN TU TRONG LIST DE XEM CHEN DC HAY CHUA");
+                Console.WriteLine("7.   KIEM TRA 1 PHAN TU CO TON TAI TRONG LIST KO");
+                Console.WriteLine("8.   XOA PHAN TU");
+                Console.WriteLine("9.   DAO NGUOC PHAN TU TRONG LIST");
+                Console.Write("    NHAP LUA  CHON CUA BAN VAO:");
+
+                chon = int.Parse(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        khaibaokhoitaolist(); break;
+                    case 2:
+
+
+                        travecacphantutrongdanhsach();
+                        break;
+
+                    case 3:
+                        duyetcacphantucuamang();
+                        break;
+
+                    case 4:
+                        sapxeptheothututangdan();
+                        break;
+                    case 5:
+                        inphantutronglist();
+                        break;
+                    case 6:
+                        ingiatriduocchen();
+                        break;
+                    case 7:
+                        kiemtra1phantutronglistcotontaihayko();
+                        break;
+                    case 8:
+                        xoaphantu();
+                        break;
+                    case 9:
+                        daonguocphantu();
+                        break;
+                    default:
+                        Console.WriteLine("vui long nhap lai");
+                        break;
+                }
+            } while (chon != 9);
+            Console.ReadLine();
+        }
+    }
+}
